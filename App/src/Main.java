31d84008071658f329baf884f188d8a8a5fe44b7
AppiliTeja20 <appilitejarayal81@gmail.com>
2026-03-26 14:13:12 +0530

    UC1

---
+++ App/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Initial bogie count: " + trainConsist.size());
+        System.out.println("Current Train Consist: " + trainConsist);
+    }
+}
