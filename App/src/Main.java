77f511fe10f226ccd8a29b442f38cdb0f3f958e2
Veeranjaneya45 <pp0238@srmist.edu.in>
2026-04-06 14:05:10 +0530

    UC5 Ready

--- App/src/Main.java
+++ App/src/Main.java
@@ -3,19 +3,19 @@ import java.util.LinkedHashSet;
 public class Main {
     public static void main(String[] args) {
 
-        // Create LinkedHashSet to store bogies
+
         LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
 
-        // Adding bogies
+
         trainFormation.add("Engine");
         trainFormation.add("Sleeper");
         trainFormation.add("Cargo");
         trainFormation.add("Guard");
 
-        // Attempt to add duplicate bogie
-        trainFormation.add("Sleeper"); // Duplicate - will be ignored
 
-        // Display final formation
+        trainFormation.add("Sleeper");
+
+
         System.out.println("Final Train Formation (Insertion Order Preserved):");
         for (String bogie : trainFormation) {
             System.out.print(bogie + " -> ");
