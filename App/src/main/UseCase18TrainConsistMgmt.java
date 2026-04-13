671a54597be1b29a0c183d0d7f89653cbda2a2f8
AppiliTeja20 <appilitejarayal81@gmail.com>
2026-04-13 16:18:19 +0530

    feature/UC18

--- App/src/main/UseCase18TrainConsistMgmt.java
+++ App/src/main/UseCase18TrainConsistMgmt.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class UseCase17TrainConsistMgmt {
+public class UseCase18TrainConsistMgmt {
 
     public static void sortBogieNames(String[] arr) {
         Arrays.sort(arr);
